8b19ff8272a9d0cf629a691cf98f181ef001d355
AlinaTan77 <102473618+AlinaTan77@users.noreply.github.com>
2024-03-26 15:37:59 -0400

    Create Trun_Run.m

---
+++ Trun_Run.m
@@ -0,0 +1,9 @@
+%Assignment 2 Lydia and Alina
+%Analysis
+%Find the features to determine the avoidance
+%Build the algorithm to explain this data
+%%
+%load the data
+data_h2o = load("JB_JAABA2/o_five_H2O_0s1x900s0s#n#n#n@40/data.mat");
+data_EA5 = load ("JB_JAABA2/o_five_10n5EA_0s1x900s0s#n#n#n@40/data.mat");
+data_GA1 = load("JB_JAABA1/o_five_10n1GA_0s1x900s0s#n#n#n@40/data.mat");
