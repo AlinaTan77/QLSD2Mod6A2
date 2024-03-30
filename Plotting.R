6710f5695d0af8fda8c0098906d7633f81d62d72
AlinaTan77 <102473618+AlinaTan77@users.noreply.github.com>
2024-03-30 17:24:37 -0400

    Update Plotting.R

--- Plotting.R
+++ Plotting.R
@@ -1,3 +1,5 @@
+setwd("~/Desktop/QLS_600/Mod6/A2")
+
 ##### Heading vs Turn Frequency
 file <- 'turn_freq.csv'
 TurnFreqDF <- read.csv(file, stringsAsFactors = FALSE)
@@ -36,3 +38,28 @@ library(ggplot2)
 ggplot(TurnFreqDF, aes(x = Heading, y = Head_Change)) +
   geom_ribbon(aes(ymin = Head_Change - Err, ymax  = Head_Change + Err), alpha = 0.2) +
   geom_line()
+
+#### Plot Histogram: Head direction after turn
+after_turn1<- read.csv("post_degree1.csv",header = FALSE)
+after_turn2<- read.csv("post_degree2.csv",header = FALSE)
+after_turn3<- read.csv("post_degree3.csv",header = FALSE)
+after_turn4<- read.csv("post_degree4.csv",header = FALSE)
+
+after_turn1_t <-t(after_turn1)
+after_turn2_t <-t(after_turn2)
+after_turn3_t <-t(after_turn3)
+after_turn4_t <-t(after_turn4)
+
+### Plot the histogram
+par(mfrow = c(2, 2))
+
+hist(after_turn1_t, main = "Histogram of Head Direction After Turn (-45,45)", 
+     xlab = "Degree", ylab = "Frequency")
+hist(after_turn2_t, main = "Histogram of Head Direction After Turn (45,135)", 
+     xlab = "Degree", ylab = "Frequency")
+hist(after_turn3_t, main = "Histogram of Head Direction After Turn (-135, -45)", 
+     xlab = "Degree", ylab = "Frequency")
+hist(after_turn4_t, main = "Histogram of Head Direction After Turn (-135, 135)", 
+     xlab = "Degree", ylab = "Frequency")
+
+par(mfrow = c(1, 1))
